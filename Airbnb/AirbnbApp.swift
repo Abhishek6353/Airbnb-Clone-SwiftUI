84e46259efc00507fd460d39b83d8f6203db8537
Abhishek <itsabhi814@gmail.com>
2025-01-08 00:20:13 +0530

    - initial commit

---
+++ Airbnb/AirbnbApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirbnbApp.swift
+//  Airbnb
+//
+//  Created by Abhishek on 08/01/25.
+//
+
+import SwiftUI
+
+@main
+struct AirbnbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Airbnb/Core/Explore/ExploreView.swift
@@ -0,0 +1,24 @@
+//
+//  ExploreView.swift
+//  Airbnb
+//
+//  Created by Abhishek on 08/01/25.
+//
+
+import SwiftUI
+
+struct ExploreView: View {
+    var body: some View {
+        NavigationStack {
+            ScrollView {
+                LazyVStack {
+                    
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    ExploreView()
+}
